1701c0b0b52ce79437c1d49d4555935aa1d229d2
araobp <araobp@users.noreply.github.com>
2025-02-03 18:11:16 +0900

    first commit

---
+++ gemini/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Gemini</h1>
+
+<p>
+Query: {prompt}
+</p>
+
+<p>
+Answer: {answer}
+</p>
+
+<script>
+    import { PUBLIC_GEMINI_API_KEY } from "$env/static/public";
+    import { GoogleGenerativeAI } from "@google/generative-ai";
+
+    var answer = $state("");
+    var prompt = $state("Am I smart?");
+
+    const genAI = new GoogleGenerativeAI(PUBLIC_GEMINI_API_KEY);
+    //console.log(PUBLIC_GEMINI_API_KEY);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const test = async () => {
+        const result = await model.generateContent(prompt);
+        answer = result.response.text();
+    };
+
+    test();
+</script>
