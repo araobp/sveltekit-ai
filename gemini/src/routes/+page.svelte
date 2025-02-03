cfe756070cce6a5c11c5eba9c20cf62577a8c82b
araobp <araobp@users.noreply.github.com>
2025-02-03 23:15:07 +0900

    update

--- gemini/src/routes/+page.svelte
+++ gemini/src/routes/+page.svelte
@@ -1,43 +1,18 @@
-<script>
-    import { PUBLIC_GEMINI_API_KEY } from "$env/static/public";
-    import { GoogleGenerativeAI } from "@google/generative-ai";
-    import showdown from 'showdown';
+<div class="container">
+    <nav>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+    </nav>
 
-    const converter = new showdown.Converter();
-
-    var answer = $state("");
-    var prompt = $state("Am I smart?");
-
-    const genAI = new GoogleGenerativeAI(PUBLIC_GEMINI_API_KEY);
-
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const test = async () => {
-        const result = await model.generateContent(prompt);
-        const text = result.response.text();
-        const html = converter.makeHtml(text);
-        answer = html;
-    };
-
-    test();
-</script>
-
-<h1>Gemini 1.5 Flash</h1>
-
-<table class="table">
-    <thead>
-        <tr>
-            <th>Query</th>
-            <th>Response</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>{prompt}</td>
-            <td>{@html answer}</td>
-        </tr>
-    </tbody>
-</table>
+    <h1>Home</h1>
+    <div class="list-group">
+        <a href="/gemini" class="list-group-item list-group-item-action"
+            >Gemini</a
+        >
+        <a href="/about" class="list-group-item list-group-item-action">About</a
+        >
+    </div>
+</div>
 
 <style>
     @import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css";
---
+++ gemini/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container">
+    <nav>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+    </nav>
+
+    <h1>About</h1>
+</div>
