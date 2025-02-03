edee8ef185abf8865b4f54711d8fca2b5072e9b2
araobp <araobp@users.noreply.github.com>
2025-02-03 20:35:58 +0900

    update

---
+++ threlte/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Canvas } from "@threlte/core";
+    import Scene from "./Scene.svelte";
+</script>
+
+<h1>Threlte</h1>
+
+<Canvas>
+    <Scene />
+</Canvas>
+
+<style>
+    @import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css";
+</style>
