3dd5ca7e90da33791c453de2740c71fefcdf1197
araobp <araobp@users.noreply.github.com>
2025-02-22 09:51:55 +0900

    update

---
+++ gemini/src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script>
+    import "bootstrap/dist/css/bootstrap.min.css";
+    var { children } = $props();
+
+</script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <span class="navbar-brand" href="#">Gemini</span>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/image_recognition"
+                        >Image Recognition</a
+                    >
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/object_detection"
+                        >Object Detection</a
+                    >
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+<div class="container mb-3">
+    {@render children()}
+</div>
---
+++ gemini/src/routes/image_recognition/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+    import { GoogleGenerativeAI } from "@google/generative-ai";
+    import showdown from "showdown";
+    import { onMount } from "svelte";
+
+    import DropImage from "$lib/DropImage.svelte";
+    import MessageModal from "$lib/MessageModal.svelte";
+    import { SPINNER } from "$lib/bootstrap-html";
+
+    var ApiKeyInput;
+    var s_GeminiApiKey = $state("");
+    const GEMINI_API_KEY_STORAGE_KEY = "gemini-api-key";
+
+    var s_Modal = $state();
+
+    onMount(() => {
+        ApiKeyInput.onchange = (e) => {
+            const apiKey = e.target.value;
+            localStorage.setItem(GEMINI_API_KEY_STORAGE_KEY, apiKey);
+            s_GeminiApiKey = e.target.value;
+        };
+
+        const apiKey = localStorage.getItem("gemini-api-key");
+        if (apiKey !== null) {
+            ApiKeyInput.value = apiKey;
+            s_GeminiApiKey = apiKey;
+        }
+    });
+
+    var model = $derived.by(() => {
+        const genAI = new GoogleGenerativeAI(s_GeminiApiKey);
+        return genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    });
+
+    const MAX_SIZE = 5 * 1024 * 1024; // 5MB
+    const DEFAULT_IMAGE = "/favicon.png";
+
+    var s_Answer = $state();
+
+    const converter = new showdown.Converter();
+
+    const generateContentWithGemini = async (b64Image, prompt) => {
+        const data = b64Image.split(",")[1].trim();
+        const mimeType = b64Image.split(";")[0].split(":")[1];
+
+        const result = await model.generateContent([
+            {
+                inlineData: {
+                    data: data,
+                    mimeType: mimeType,
+                },
+            },
+            prompt,
+        ]);
+
+        return result.response.text();
+    };
+
+    const describe = async (b64Image) => {
+        s_Modal.show();
+        s_Answer = "";
+        const answer = await generateContentWithGemini(
+            b64Image,
+            "Describe the image",
+        );
+        s_Answer = converter.makeHtml(answer);
+        s_Modal.hide();
+    };
+</script>
+
+<h1>Image Recognition</h1>
+
+<div class="input-group w-50 mt-3">
+    <span class="input-group-text" id="">Gemini API Key</span>
+    <input type="text" class="form-control" bind:this={ApiKeyInput} />
+</div>
+
+<div class="d-flex mt-3">
+    <div class="w-25 p-2">
+        <DropImage
+            maxSize={MAX_SIZE}
+            defaultImage{DEFAULT_IMAGE}
+            callback={describe}
+        ></DropImage>
+    </div>
+    <div class="w-75 p-2">
+        {@html s_Answer}
+    </div>
+</div>
+
+<MessageModal title="Processing..." innerHTML={SPINNER} bind:modal={s_Modal}
+></MessageModal>
