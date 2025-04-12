12d574782a9535120b231fb65d4151b7c5e0cb0b
araobp <araobp@users.noreply.github.com>
2025-04-13 08:48:43 +0900

    update

--- app/src/routes/translation/+page.svelte
+++ app/src/routes/translation/+page.svelte
@@ -1,19 +1,27 @@
 <script>
+    import { geminiAPI } from "$lib/settings";
+
     var s_Text = $state();
+    var s_Lang = $state();
+    var s_Result = $state("...");
 
-    const translate = (text) => {
-
-
-        s_Text = "";
+    const translate = async (text) => {
+        s_Result = "...";
+        const prompt = `Translate the following text to ${s_Lang}.
+        
+        ${s_Text}
+        `;
+        const result = await $geminiAPI.generateContent(prompt);
+        s_Result = result.response.text();
     };
 </script>
 
 <div>
     <h3>Translation (Gemini)</h3>
 
-    <select class="form-select w-25 mt-3">
-        <option value="e_to_j">English to Japanese</option>
-        <option value="j_to_e">Japanese to English</option>
+    <select class="form-select w-25 mt-3" bind:value={s_Lang}>
+        <option value="Japanese">to Japanese</option>
+        <option value="English" selected>to English</option>
     </select>
 
     <div class="d-flex align-items-center mt-2">
@@ -22,15 +30,17 @@
             class="form-control w-100"
             placeholder="Enter text..."
             bind:value={s_Text}
-            onkeypress={e => {
-                if (e.key === 'Enter') {
-                    speak();
+            onkeypress={(e) => {
+                if (e.key === "Enter") {
+                    translate();
                 }
             }}
-
-/>
+        />
         <button class="btn btn-primary ms-2" onclick={translate}>
-            Speak
+            Translate
         </button>
     </div>
+
+    <textarea class="w-100 mt-2 rounded border p-2" bind:value={s_Result}
+    ></textarea>
 </div>
